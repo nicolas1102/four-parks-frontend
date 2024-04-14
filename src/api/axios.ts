2942ac185d2cebb220cb6a20d513101ab7c2a114
adrianadiazv <nicolasdiazv1102@gmail.com>
2024-04-13 21:49:42 -0500

    eliminando basura

--- src/api/axios.ts
+++
@@ -1,8 +0,0 @@
-import axios from 'axios'
-
-const instance = axios.create({
-    baseURL: "http://localhost:3001/api",
-    withCredentials: true
-})
-
-export default instance
--- src/api/equipoAPI.ts
+++
@@ -1,8 +0,0 @@
-import { Equipo } from '../../types'
-import axios from './axios'
-
-export const getEquiposRequest = async () => await axios.get('/equipos')
-export const getEquipoRequest = (id: string) => axios.get(`/equipos/${id}`)
-export const createEquiposRequest = (equipo: Equipo) => axios.post('/equipos', equipo)
-export const updateEquiposRequest = (id: string, equipo: Equipo) => axios.put(`/equipos/${id}`, equipo)
-export const deleteEquiposRequest = (id: string) => axios.delete(`/equipos/${id}`)
