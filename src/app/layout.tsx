8aacdc0a0801f9c3d5781072db357f324aeab3f8
adrianadiazv <nicolasdiazv1102@gmail.com>
2024-04-14 22:03:53 -0500

    commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,55 @@
+import type { Metadata } from 'next'
+import { Inter as FontSans } from 'next/font/google'
+import './globals.css'
+import { ThemeProvider } from '@/hooks/useThemeProvider'
+import { cn } from '@/lib/utils'
+import Loader from '@/components/Loader'
+import { Suspense } from 'react'
+import { Toaster } from '@/components/ui/sonner'
+import { DEFAULT_SITE_DESCRIPTION, DEFAULT_SITE_TITLE } from './const'
+import Navbar from '@/components/layout/navbar/Navbar'
+import Footer from '@/components/layout/Footer'
+
+
+export const metadata: Metadata = {
+  title: DEFAULT_SITE_TITLE,
+  description: DEFAULT_SITE_DESCRIPTION,
+}
+
+export const fontSans = FontSans({
+  subsets: ['latin'],
+  variable: '--font-sans',
+})
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang='en'>
+      <body
+        className={cn(
+          'min-h-screen bg-background font-sans antialiased',
+          fontSans.variable
+        )}
+      >
+        <ThemeProvider
+          attribute='class'
+          defaultTheme='system'
+          enableSystem
+          disableTransitionOnChange
+        >
+          <main className='relative flex flex-col min-h-screen'>
+            <Navbar />
+            <Suspense fallback={<Loader />}>
+              <div className='mx-10 mt-10 mb-20'>{children}</div>
+            </Suspense>
+            <Footer />
+          </main>
+          <Toaster />
+        </ThemeProvider>
+      </body>
+    </html>
+  )
+}
