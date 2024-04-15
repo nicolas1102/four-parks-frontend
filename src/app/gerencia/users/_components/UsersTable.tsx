cdc17c2ca531f8452336db9f1543c4a320e49f06
adrianadiazv <nicolasdiazv1102@gmail.com>
2024-04-14 20:20:20 -0500

    centrando tabla de usuarios

--- src/app/gerencia/users/_components/UsersTable.tsx
+++ src/app/gerencia/users/_components/UsersTable.tsx
@@ -50,7 +50,7 @@ export function UsersTable<TData, TValue>({
           <TableRow key={headerGroup.id}>
             {headerGroup.headers.map((header) => {
               return (
-                <TableHead key={header.id}>
+                <TableHead key={header.id} className='text-center'>
                   {header.isPlaceholder
                     ? null
                     : flexRender(
@@ -72,7 +72,7 @@ export function UsersTable<TData, TValue>({
               data-state={row.getIsSelected() && 'selected'}
             >
               {row.getVisibleCells().map((cell) => (
-                <TableCell key={cell.id}>
+                <TableCell key={cell.id} className='text-center'>
                   {flexRender(cell.column.columnDef.cell, cell.getContext())}
                 </TableCell>
               ))}
