26fd124863ef60bb99cb78fe97ea2824606368d5
adrianadiazv <nicolasdiazv1102@gmail.com>
2024-04-18 22:42:49 -0500

    changing navbar

--- src/components/Layout/Navbar/NavItem.tsx
+++ src/components/Layout/Navbar/NavItem.tsx
@@ -9,11 +9,10 @@ const NavItem = ({ title, link }: NavItemInterface) => {
   return (
     <Link
       className={
-        'select-none space-y-1 p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground flex flex-row items-center gap-1 tracking-widest border hover:border-primary hover:bg-yellow-200  dark:hover:bg-yellow-300 dark:hover:text-background'
+        'select-none space-y-1 p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground flex flex-row items-center gap-1 tracking-widest border border-primary bg-yellow-300 hover:bg-yellow-200 dark:hover:bg-yellow-300 dark:hover:text-background'
       }
       href={link}
     >
-
       <p className='text-sm font-medium leading-none m-0'>{title}</p>
     </Link>
   )
--- src/components/Layout/Navbar/Navbar.tsx
+++ src/components/Layout/Navbar/Navbar.tsx
@@ -11,13 +11,7 @@ export const PAGES_INFO = [
     link: '/parqueaderos',
     icon: 'Player',
     text: 'Gestiona mejores jugadores de la historia del futbol.',
-  },
-  {
-    title: 'SOBRE NOSOTROS',
-    link: '/about-us',
-    icon: 'Shield',
-    text: 'Gestiona los equipos más grandes del mundo.',
-  },
+  }
 ]
 
 const Navbar = async () => {
@@ -25,23 +19,26 @@ const Navbar = async () => {
   return (
     <div className='flex flex-col sticky z-50 top-0 inset-x-0 items-center justify-center w-full border-b-2 backdrop-blur supports-[backdrop-filter]:bg-background/60'>
       <div className='w-full text-center p-1 bg-yellow-300'></div>
-      <div className='flex flex-row justify-around w-full p-1 content-center '>
-        <ModeToggle />
-        <div className='flex flex-row justify-around w-full'>
-          <NavItem title={PAGES_INFO[0].title} link={PAGES_INFO[0].link} />
+
+      <div className='flex flex-row justify-between w-full p-1 content-center gap-2'>
+        <div className='flex flex-row px-1'>
           <Link href='/' className='flex flex-row justify-center'>
-            <p className='text-3xl font-medium tracking-wider text-center text-primary sm:text-4xl pr-4'>
+            <p className='text-3xl font-medium tracking-wider text-center text-primary sm:text-4xl pr-2'>
               FOUR
             </p>
-            <Icons.logo className=' w-11 h-11 text-yellow-400' />
+            <Icons.logo className=' w-11 h-11 text-yellow-400 hover:-rotate-45 transition-all' />
             <p className=' text-3xl font-medium tracking-wider text-center text-primary sm:text-4xl'>
               ARKS
             </p>
           </Link>
-          <NavItem title={PAGES_INFO[1].title} link={PAGES_INFO[1].link} />
         </div>
-        {/* TODO: Add auth logic */}
-        <ProfileNav isLoggedIn={session ? true : false} />
+        <div className='flex flex-row justify-end gap-2 px-1'>
+          <NavItem title={PAGES_INFO[0].title} link={PAGES_INFO[0].link} />
+          <span className='border'></span> 
+          <ProfileNav isLoggedIn={session ? true : false} />
+          <span className='border'></span> 
+          <ModeToggle />
+        </div>
       </div>
     </div>
   )
