5f80b4b6e6d89a87a284587e6e2cffc6777c76d0
adrianadiazv <nicolasdiazv1102@gmail.com>
2024-05-19 09:17:10 -0500

    Update VehicleTypePieChart.tsx

--- src/app/admin/parqueaderos/[parkingId]/estadisticas/_components/VehicleTypePieChart.tsx
+++ src/app/admin/parqueaderos/[parkingId]/estadisticas/_components/VehicleTypePieChart.tsx
@@ -36,8 +36,8 @@ export function VehicleTypePieChart() {
           </CardDescription>
         </div>
       </CardHeader>
-      <CardContent className='h-[400px]'>
-        <ResponsiveContainer width='100%' height='100%'>
+      <CardContent className='h-[400px] flex justify-center'>
+        <ResponsiveContainer width='80%' height='100%'>
           <PieChart width={400} height={400}>
             <Legend />
             <Pie
@@ -46,7 +46,7 @@ export function VehicleTypePieChart() {
               data={data01}
               cx='50%'
               cy='50%'
-              outerRadius={130}
+              outerRadius={110}
               fill='#8884d8'
               label
             >
